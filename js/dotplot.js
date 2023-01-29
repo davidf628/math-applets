55784ce098388ccaa65681581d7e10e90fa9cbf6
davidf628 <30780186+davidf628@users.noreply.github.com>
2023-01-29 10:18:17 -0500

    Completed Axis and labels of chart

--- js/dotplot.js
+++ js/dotplot.js
@@ -11,19 +11,19 @@
  *      scale - an object of the form:
  *           { xmin, xmax, scale } - scale to draw the plot within      
  *      data - an array of numbers to plot
- *      parameters (optional) - a JSON object of pre-defined constants to
+ *      args (optional) - a JSON object of pre-defined constants to
  *        modify the way the dot plot is drawn
  */
 
 import { rectangle } from './rectangle.js';
 
-export function dotplot(board, bounds, scale, data, parameters) {
+export function dotplot(board, bounds, scale, data, args) {
 
-    // Check to see if any parameters were specified, make a default empty
+    // Check to see if any args were specified, make a default empty
     //   object if not
 
-    if (parameters === undefined) {
-        parameters = {};
+    if (args === undefined) {
+        args = {};
     }
 
     // Make sure the bounds and scale were specified, or else exit
@@ -42,13 +42,41 @@ export function dotplot(board, bounds, scale, data, parameters) {
         return
     }
 
-    // Set all the base parameters
+    // Set all the base args
+    let x = bounds.x;
+    let axisy = bounds.y - 0.8 * bounds.height; // Axis to be positioned 20% from the bottom
+    let width = bounds.width;
 
-    let border = parameters.border ? parameters.border : false;
-    let borderColor = parameters.borderColor ? parameters.borderColor : 'black';
+    let border = args.border ? args.border : false;
+    let borderColor = args.borderColor ? args.borderColor : 'black';
 
     if (border) {
-        rectangle(board, bounds, {border: border, borderColor: borderColor });
+        rectangle(board, bounds, {border: border, borderColor: borderColor, showFill: true, fillOpacity: 1, fillColor: 'white' });
+    }
+
+    // Draw the axis
+    board.create('line', [[x, axisy], [x+width, axisy]], {
+        straightFirst: false,
+        straightLast: false,
+        firstArrow: true,
+        lastArrow: true,
+    });
+
+    // Get board pixels per unit
+    let box = board.getBoundingBox();
+    let xppu = board.canvasWidth / (box[2] - box[0]);
+    let yppu = board.canvasHeight / (box[1] - box[3]);
+
+
+    // Draw the tick marks and labels
+    let nMarks = Math.ceil((scale.xmax - scale.xmin) / scale.scale);
+    let xDist = bounds.width / (nMarks + 2); // Space out the tick marks
+    let tickHeight = 5 / yppu; // Make tick marks about 10 pixels
+    let labelStart = 10 / yppu; // Make labels start about 10 pixes below axis
+    for(let i = 0; i <= nMarks; i++) {
+        board.create('segment', [[x + (i+1)*xDist, axisy+tickHeight],[x + (i+1)*xDist, axisy-tickHeight]]);
+        let label = Math.round(scale.xmin + i * scale.scale).toString();
+        board.create('text', [x + (i+1)*xDist, axisy-labelStart, label], {anchorX: 'middle'})
     }
 
 
--- js/rectangle.js
+++ js/rectangle.js
@@ -1,4 +1,18 @@
-export function rectangle(board, bounds, parameters) {
+
+/******************************************************************************
+ * 
+ * Draws a rectangle on a JSX board, given specified bounds.
+ * 
+ * Inputs:
+ *   board - the jsx board to draw on
+ *   bounds - an object specifying the bounds of the rectangle, must
+ *     be of the form: {x, y, width, height}
+ *   args (optional) - a JSON object that contains constants controlling
+ *     how the rectangle should be drawn
+ * 
+ */
+
+export function rectangle(board, bounds, args) {
 
     // Exit if no board is specified
 
@@ -10,11 +24,11 @@ export function rectangle(board, bounds, parameters) {
     // Check to see if any parameters were specified, make a default empty
     //   object if not
 
-    if (parameters === undefined) {
-        parameters = {};
+    if (args === undefined) {
+        args = {};
     }
 
-    // Make sure the bounds and scale were specified, or else exit
+    // Make sure the bounds were specified, or else exit
 
     if (bounds === undefined || bounds.x === undefined || bounds.y === undefined 
         || bounds.width === undefined || bounds.height === undefined) {
@@ -23,23 +37,44 @@ export function rectangle(board, bounds, parameters) {
         return
     }
 
-    let borderColor = parameters.borderColor ? parameters.borderColor : 'black';
+    let borderColor = args.borderColor ? args.borderColor : 'black';
+    let showBorder = args.showBorder ? args.showBorder : true;
+    let borderWidth = args.borderWidth ? args.borderWidth : 1;
+    let borderOpacity = args.borderOpacity ? args.borderOpacity : 1;
+    let showFill = args.showFill ? args.showFill : false;
+    let fillColor = args.fillColor ? args.fillColor : 'green';
+    let fillOpacity = args.fillOpacity ? args.fillOpacity : 0.2;
 
     let x = bounds.x;
     let y = bounds.y;
     let w = bounds.width;
     let h = bounds.height;
 
-    var poly = board.create('polygon', [[x, y], [x+w, y], [x+w, y+h], [x, y+h]]);
+    var poly = board.create('polygon', [[x, y], [x+w, y], [x+w, y-h], [x, y-h]]);
+
     poly.setAttribute({ highlight: false});
     poly.setAttribute({ fixed: true });
+
+    if (showFill) {
+        poly.setAttribute({ fillColor: fillColor });
+        poly.setAttribute({ fillOpacity: fillOpacity });
+    } else {
+        poly.setAttribute({ fillOpacity: 0 });
+    }
+
     for(let point of poly.vertices) {
         point.setAttribute({ visible: false });
     }
-    for(let border of poly.borders) {
-        border.setAttribute({ strokeColor: borderColor });
-        border.setAttribute({ fixed: true });
-        border.setAttribute({ highlight: false });
+
+    if (showBorder) {
+
+        poly.setAttribute({ strokeWidth: borderWidth });
+        poly.setAttribute({ strokeOpacity: borderOpacity });
+        for(let border of poly.borders) {
+            border.setAttribute({ strokeColor: borderColor });
+            border.setAttribute({ fixed: true });
+            border.setAttribute({ highlight: false });
+        }
     }
 
 
