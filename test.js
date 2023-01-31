32cf6bcd69ef740534a359af4c11887200eec7d1
davidf628 <30780186+davidf628@users.noreply.github.com>
2023-01-31 07:13:05 -0500

    Nothing much to see here

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-import { round } from './math.js';
-import { randomNormalData } from './stats.js';
+import { round } from './js/math.js';
+import { randomNormalData } from './js/stats.js';
 
 console.log(round(25.5));
 console.log(randomNormalData(15, 10, 25));
