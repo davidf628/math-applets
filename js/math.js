b26415e61b09a70dc089436f6f18861022366071
davidf628 <30780186+davidf628@users.noreply.github.com>
2023-01-31 06:57:41 -0500

    Removed typescript

--- js/math.js
+++ js/math.js
@@ -9,7 +9,6 @@
 exports.__esModule = true;
 exports.log = exports.ln = exports.even = exports.odd = exports.sgn = exports.round = exports.swap = void 0;
 /******************************************************************************
- *
  * This function swaps two numbers contained in different variables.
  */
 function swap(a, b) {
@@ -27,24 +26,19 @@ function sgn(a) {
     return a / Math.abs(a);
 }
 exports.sgn = sgn;
-;
 function odd(x) {
     return (x % 2) == 1;
 }
 exports.odd = odd;
-;
 function even(x) {
     return (x % 2) == 0;
 }
 exports.even = even;
-;
 function ln(x) {
     return Math.log(x);
 }
 exports.ln = ln;
-;
 function log(x) {
     return Math.log(x) / Math.log(10);
 }
 exports.log = log;
-;
